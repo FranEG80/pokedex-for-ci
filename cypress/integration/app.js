3e1c2755c9d5b10bf65ecf3ddf8160e6d75f5023
FranEG80 <fjenriglez@gmail.com>
2021-08-08 21:38:27 +0200

    adding heroku deploy

--- cypress/integration/app.js
+++ cypress/integration/app.js
@@ -1,7 +1,9 @@
-// /* global cy */
+/* global cy */
 
-// describe('Pokedex', function () {
-//   it('frongpage can be opened', function () {
-//     cy.visit('http://localhost:5000/')
-//   })
-// })
+describe('Pokedex', function () {
+  it('frontpage can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('pikachu')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo')
+  })
+})
